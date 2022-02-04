b799b2827f147b782140a5b0fd23639ac25b1829
Maxim <maxrozhkov1@gmail.com>
2022-02-04 15:22:45 +0300

    work1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let d;
+    d = b**2 - 4*a*c;
+    if (d < 0) {
+      arr = [];
+    } else if (d === 0) {
+      let x;
+      x = -b / (2 * a);
+      arr = [x];
+    } else if (d > 0) {
+      let x1;
+      let x2;
+      x1 = (-b + Math.sqrt(d))/(2*a)
+      x2 = (-b - Math.sqrt(d))/(2*a)
+      arr = [x1,x2];
+    }
   return arr; // array
 }
 
