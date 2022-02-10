7b69a56351259fb54bbae6b09002efce91c66b3f
Maxim <maxrozhkov1@gmail.com>
2022-02-11 00:05:13 +0300

    work3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length === arr2.length && arr1.every((numb, i) => numb === arr2[i])) {
+      result = true;
+  } else {
+    result = false;
+  }
 
-  return result; // boolean
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+   resultArr = arr.filter((positiveNumbers) => positiveNumbers > 0).filter((positiveNumbers) => positiveNumbers % 3 === 0).map((positiveNumbers) => positiveNumbers * 10);
 
   return resultArr; // array
 }
