9009bc6d7594181d246d57d3ffe7a5a0eebbbef7
Maxim <maxrozhkov1@gmail.com>
2022-02-16 23:56:26 +0300

    work4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.grades = {};
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){
+      this.marks = [mark];
+      } else {
+          this.marks.push(mark);
+      }
+}
+
+Student.prototype.addMarks = function (...marksAll) {
+      this.marks = marksAll;
+}
+
+Student.prototype.getAverage = function () {
+  let course_count = 0;
+  let sum = 0;
+    for (let course in this.marks) {
+      sum += course;
+    }
+    sum = sum / this.marks.length;
+    return Number(sum.toFixed());
+}
+
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
+
+Number(
