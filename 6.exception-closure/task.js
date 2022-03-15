fe0e326c8a36255fdf324c8440be7a2e36d3b805
colins1 <88490207+colins1@users.noreply.github.com>
2022-03-15 21:41:18 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,3 +16,35 @@ function validateCount(number) {
         return e;
     }
 }
+
+class Triangle {
+  constructor (a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b < c || a + c < b || c + b < a) {
+      const divideError1 = new Error("Треугольник с такими сторонами не существует");
+      throw divideError1;
+    }
+  }
+
+  getPerimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  getArea() {
+    let p = (this.a + this.b + this.c) / 2;
+    let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    return Number(s.toFixed(3));
+  }
+}
+
+function getTriangle (a, b, c) {
+  try {
+      const tria = new Triangle(a, b, c);
+      return tria;
+    }
+    catch (e1) {
+      return e1;
+    }
+}
