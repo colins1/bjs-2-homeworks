e63b082f74faace868c070a7ca2cb2bb776ea92f
colins1 <88490207+colins1@users.noreply.github.com>
2022-03-18 17:27:52 +0300

    Update task.js

---
+++ 7.async/task.js
@@ -0,0 +1,14 @@
+class AlarmClock {
+  constructor(alarmCollection, timerId) {
+    this.alarmCollection = [];
+    this.timerId = timerId;
+  }
+
+  addClock (id, time, callback) {
+    if(id == undefined) {
+      throw new Error('error text');
+    } else {
+
+    }
+  }
+}
