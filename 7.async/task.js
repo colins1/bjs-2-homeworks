a14745e70ea439a3a85d10e2d1cc9682c08f8433
colins1 <88490207+colins1@users.noreply.github.com>
2022-03-21 10:14:32 +0300

    Update task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -4,11 +4,40 @@ class AlarmClock {
     this.timerId = timerId;
   }
 
-  addClock (id, time, callback) {
-    if(id == undefined) {
+  addClock (time, callback, id) {
+    if(!id) {
       throw new Error('error text');
-    } else {
+    }
+    if (this.alarmCollection.id == id) {
+      console.error("Звонок уже существует");
+      return;
+    }
+    return this.alarmCollection.push({id, time, callback});
+  }
 
+  removeClock (id) {
+    if (this.alarmCollection.filter(word => word.id == id)) {
+      const index = this.alarmCollection.findIndex(book => book.id === id);
+      return this.alarmCollection.splice(index, 1);
+    } else {
+      return arr;
+    }
+
+  }
+
+  getCurrentFormattedTime () {
+    let a = String(this.alarmCollection.time);
+    return a;
+  }
+
+  start (checkClock) {
+    if (String(checkClock) === String(this.alarmCollection.time)) {
+    let b = this.alarmCollection.callback();
+    return b;
     }
   }
+
 }
+
+
+//let now = new Date().toLocaleTimeString().slice(0,-3); // 11:02
