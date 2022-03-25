c77ce20d0aed72884521a1bc47c5d88ad1807148
colins1 <88490207+colins1@users.noreply.github.com>
2022-03-25 21:46:28 +0300

    Update task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -8,22 +8,23 @@ class AlarmClock {
     if(!id) {
       throw new Error('error text');
     }
-    if (this.alarmCollection.id == id) {
+    if (this.timerId == id) {
       console.error("Звонок уже существует");
       return;
     }
+    this.timerId = id;
     return this.alarmCollection.push({id, time, callback});
   }
+
 
   removeClock (id) {
     if (this.alarmCollection.filter(word => word.id == id)) {
       const index = this.alarmCollection.findIndex(book => book.id === id);
-      let a = this.alarmCollection.splice(index, 1);
+      this.alarmCollection.splice(index, 1);
       return true;
     } else {
       return false;
     }
-
   }
 
   getCurrentFormattedTime () {
@@ -31,16 +32,41 @@ class AlarmClock {
   }
 
   start () {
-    const checkIdAlarm = (checkClock) => {
-      if (checkClock.time == getCurrentFormattedTime()) {
-        checkClock.callback();
+    const checkClock = (checkIdAlarm) => {
+      if (checkIdAlarm.time == this.getCurrentFormattedTime()) {
+        checkIdAlarm.callback();
       }
       if (!this.timerId) {
-        const allAlarm = () => this.alarmCollection.every((numb, i) => checkClock(numb))
-        this.timerId = setInterval(allAlarm, 1000);
+        const allAlarm = this.alarmCollection.filter(function(number) {
+          checkClock(number);
+        });
+        let a = setInterval(allAlarm, 1000);
+        return a;
       }
     };
-    checkClock(this.alarmCollection[0]);
   }
 
+  stop () {
+    if (this.timerId) {
+      clearInterval(this.start());
+    }
+  }
+
+  printAlarms () {
+    this.alarmCollection.forEach(element => console.log(`Айди ${element.id}!`));
+  }
+
+  clearAlarms() {
+    stop();
+    for (;;) {
+        this.removeClock(this.alarmCollection[0].id);
+        if(!this.alarmCollection[0]) {
+          break;
+        }
+    }
+  }
 }
+
+
+
+//let now = new Date().toLocaleTimeString().slice(0,-3); // 11:02
