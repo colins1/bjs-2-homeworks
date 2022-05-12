5fa3f494161031bde4a7368b328be807e29f371c
colins1 <88490207+colins1@users.noreply.github.com>
2022-05-12 18:34:59 +0300

    add

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,7 +1,12 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = []
+  function wrapper (...arg) {
+    cache.push({hash: arg.toString(), value: func(...arg)})
+    let result = func(...arg)
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
 }
-
 
 function debounceDecoratorNew(func) {
   // Ваш код
